feaf20d64eafd9959d5f1dfacbf16f659c414ccf
Volodymyr But <git@hiegz.com>
2026-01-02 23:34:17 +0100

    restructure the project as a monorepo

--- apps/homepage/next.config.ts
+++ apps/homepage/next.config.ts
@@ -2,9 +2,6 @@ import * as path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    turbopack: {
-        root: path.join(__dirname, "."),
-    },
     images: {
         remotePatterns: [new URL("https://placehold.co/**")],
     },
