5763fcd0e362ab0bba121bf5c086650af10b3895
Volodymyr But <git@hiegz.com>
2026-01-03 12:36:12 +0100

    move dropdown to @dach/ui

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,2 +1,3 @@
 export { default as Carousel } from "./src/carousel";
 export { default as CarouselSlide } from "./src/carousel-slide";
+export { default as Dropdown, type DropdownRef } from "./src/dropdown";
